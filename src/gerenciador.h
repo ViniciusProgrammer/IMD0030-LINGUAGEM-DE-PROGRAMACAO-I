8022d16b0159adf66660553c156a76cc894086fc
davi <viniprogrammer@hotmail.com>
2023-12-12 15:24:53 -0300

    criei o arquivo gerenciador.h

---
+++ src/gerenciador.h
@@ -0,0 +1,18 @@
+#ifndef GERENCIADOR.HPP
+#define GERENCIADOR.HPP
+
+#include <iostream>
+
+#include "produto.h"
+#include "cliente.h"
+#include "carrinho.h"
+
+class Gerenciador : public Produto, public Carrinho, public Cliente{
+
+    private:
+
+    public:
+
+};
+
+#endif
