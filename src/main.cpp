806aa4deb249425328598f56ad0ff5c31c9f4e17
davi <viniprogrammer@hotmail.com>
2023-12-13 18:52:52 -0300

    implementei um loop para escolha do usuario

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,42 @@
 #include <iostream>
 #include <string>
-#include "cliente.h"
 #include <limits>
 #include <vector>
-#include "carrinho.h"
+
+#include "caixaSupermercado.h"
 
 using namespace std;
 
 int main()
 {
-    vector<Produto> novoProduto;
-    novoProduto.push_back(Produto("Cigarro", "238742", 5.50, 3, 16.50));
-    Carrinho novoCarrinho("Cliente 1", "000.000.000-00", "01/01/2000", "Masculino", "Rua 1", 1, "Bairro 1", "Cidade 1", "00000-000", novoProduto);
+    Caixa caixa;
+    int opcao;
 
-    cout << "Dados da Compra:" << endl;
-    cout << "Nome: " << novoCarrinho.getNome() << endl;
-    cout << "Cidade: " << novoCarrinho.getEndereco()->getCidade() << endl;
-    cout << "Nome Produto: " << novoCarrinho.get_nomeproduto() << endl;
+    do{
+        caixa.menuCaixa();
+        cin >> opcao;
 
+        switch(opcao){
+            case 1:
+                caixa.registrarCompra();
+                break;
+            case 2:
+                caixa.listarCompras();
+                break;
+            case 3:
+                //caixa.excluirCompra();
+                break;
+            case 4:
+                //caixa.buscarCompra();
+                break;
+            case 5:
+                cout << "Encerrando Sistema..." << endl;
+                break;
+            default:
+                cout << "Opção inválida!" << endl;
+                break;
+        }
+    } while(opcao != 5);
+    
     return 0;
 }
