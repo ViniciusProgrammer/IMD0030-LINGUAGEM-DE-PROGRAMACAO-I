92dea284126fec410047dde22f5940fa265c2292
davi <viniprogrammer@hotmail.com>
2023-12-13 18:48:19 -0300

    Incrementado ao contructor o id, preço e quantidade

--- src/carrinho.cpp
+++ src/carrinho.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
 #include <vector>
 
-#include "carrinho.h"
+#include "header/carrinho.h"
 
 using namespace std;
 //string nome_produto, string id_produto, float preco, int quantidade, float valor_total
-Carrinho::Carrinho(){
-    this->produtos.push_back(Produto("", "", 0.00, 0, 0.00));
-}
+Carrinho::Carrinho(){}
 
 Carrinho::Carrinho(string nome, string cpf, string dataNascimento,
-            string genero, string rua, int numero, string bairro,
-            string cidade, string cep, vector<Produto> produtos){
-    this->produtos = produtos;
-}
-
+                string genero, string rua, int numero, string bairro,
+                string cidade, string cep, string estado,
+                string nome_produto, string id_produto, float preco, int quantidade):
+                Cliente(nome, cpf, dataNascimento, genero, rua, numero, bairro, cidade, cep, estado),
+                Produto(nome_produto, id_produto, preco, quantidade){}
 
